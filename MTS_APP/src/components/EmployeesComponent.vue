679a5be5cc51767a04e55377c4479a40f831ad6f
Siza-Mpafa030628 <sizampafa972@gmail.com>
2025-12-05 22:13:18 +0200

    In EmployeesComponent.vue I imported store which is store/index.js, remember in store/index.js i imported all the json files(dummy data) including employee_info.json, in the employees component's data function workers are returned, i got these workers from the store that i imported, i targetted the  state object so i can get/access  employees then I displayed them in template, for now I am not using routes, I directly imported EmployeesComponent.vue into App.vue so i can see the employees

---
+++ MTS_APP/src/components/EmployeesComponent.vue
@@ -0,0 +1,21 @@
+<script>
+    import store from '@/store';
+    export default {
+        data(){
+            return{
+                workers: store.state.employees
+            }
+        }
+    }
+</script>
+
+<template>
+    <ul>
+        <li v-for="employee in workers" :key="employee.employeeId">{{ employee.name }}</li>
+    </ul>
+
+</template>
+
+<style>
+
+</style>
