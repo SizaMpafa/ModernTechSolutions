a9e7348e64a54cddf472f4b9ecc39b685bf05532
Nivo16 <mkolisoliyema@gmail.com>
2025-12-10 09:20:05 +0200

    Updated workers button colors

--- MTS_APP/src/components/CreateEmployee.vue
+++ MTS_APP/src/components/CreateEmployee.vue
@@ -68,7 +68,7 @@ export default {
     
 
 <!-- Modal toggle -->
-<button data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="text-black bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none btn btn-primary" type="button">
+<button data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="text-black bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none btn employeee" type="button">
     New Employee
 </button>
 
@@ -142,3 +142,8 @@ export default {
 </div>
 
 </template>
+<style scoped>
+    .employeee{
+        background: linear-gradient(135deg, #667eea 0%, #764ba2 100%)
+    }
+</style>
--- MTS_APP/src/components/EmployeesComponent.vue
+++ MTS_APP/src/components/EmployeesComponent.vue
@@ -36,7 +36,7 @@
 
                     <p class="text-lowercase  text-center font-italic">{{ employee.position }}</p>
                 </div>
-                <p class="btn btn-primary right-0">{{ employee.department }}</p>
+                <p class="btn departments right-0">{{ employee.department }}</p>
             </div>
             <div v-if="selectedEmployee && selectedEmployee.employeeId === employee.employeeId" class="popup" @click.stop="hideSelectedEmployeeDetails()">
                 <div class=" expanded d-flex">
@@ -59,6 +59,9 @@
 </template>
 
 <style>
+    .departments{
+        background: linear-gradient(135deg, #667eea 0%, #764ba2 100%)
+    }
     .expanded{
         justify-content: space-between;
         padding-left: 50px;
